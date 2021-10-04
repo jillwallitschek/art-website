792081499c14be23b247cdc4eb54bbf134324d91
wallitschek <jill@scivista.com>
2021-10-03 20:12:33 -0600

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 var cursorTrail;
 var extendedTrail = [];
+lastMouseCoord = [0,0];
 
 //listeners
 
@@ -18,17 +19,41 @@ document.addEventListener("mousemove", function(event){
 
 function trackCursor(event){
     if (!cursorTrail) return;
+    lastMouseCoord[0] = event.pageX;
+    lastMouseCoord[1] = event.pageY;
+    if (!cursorTrail.classList.contains("cursor-animation")){
+        cursorTrail.classList.add("cursor-animation");
+        cursorTrail.classList.remove("hide");
+    }
+    if (event.target.nodeName == "A" || event.target.classList.contains("hover-selection") || event.target.classList.contains("link")){
+        cursorTrail.classList.add("hide");
+        return;
+    }
+    else{
+        if (cursorTrail.classList.contains("hide")){
+            cursorTrail.classList.remove("hide");
+        }
+    }
     setTimeout(()=>{
     cursorTrail.style.left = event.pageX + "px";
     cursorTrail.style.top = event.pageY + "px";
-    makeTrail();
+    //makeTrail();
+    }, 200);
+    removeAnimationOnStationary(event);
+}
+
+function removeAnimationOnStationary(event){
+    setTimeout(()=>{
+        if (event.pageX == lastMouseCoord[0] && event.pageY == lastMouseCoord[1]){
+            cursorTrail.classList.remove("cursor-animation");
+            cursorTrail.classList.add("hide");
+        }
     }, 200);
 }
 
 function makeTrail(){
     if (!cursorTrail) return;
     if (extendedTrail.length == 0){
-        console.log('yo')
         let el = cursorTrail;
         //el.id = "cursor-trail-1";
         //document.body.append(el);
