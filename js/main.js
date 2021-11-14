45d93f2909f41d6adba1734bb589d30247fb3bd9
wallitschek <jill@scivista.com>
2021-11-14 15:53:43 -0700

    nav almost done, animation on drop down needs tweeking

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //////////////////////////variables
 //nav
-var navButton
+var navButton;
+var expandedNav;
+var showNavigation = false;
 
 //cursor
 var cursorTrail;
@@ -16,7 +18,12 @@ var homeSlidesTimer;
 document.addEventListener('DOMContentLoaded', function() {
     //nav
     navButton = document.getElementById("nav-button");
-    navButton.addEventListener("click", toggleNavigation);
+    if (navButton) navButton.addEventListener("click", toggleNavigation);
+    var navLiList = document.querySelectorAll("ul#real-nav li")
+    navLiList.forEach((li)=>{
+        li.addEventListener("click", navToURL);
+        updateNavLiCSS(li);
+    });
     
     //cursor
     cursorTrail = document.getElementById("cursor-trail");
@@ -31,8 +38,45 @@ document.addEventListener("mousemove", function(event){
 });
 
 ////////////////////navigation
+function navToURL(event){
+    let id = event.target?.id;
+    if (!id) return;
+    if (id == "home") window.location.href = "index.html";
+    else if (id == "paintings") window.location.href = "paintings.html";
+    else if (id == "pencil") window.location.href = "pencil.html";
+    else if (id == "graphite") window.location.href = "graphite.html";
+    else if (id == "websites") window.location.href = "websites.html";
+    toggleNavigation(event);
+}
+
 function toggleNavigation(event){
-    console.log(event)
+    if (!event) return;
+    if (!expandedNav) expandedNav = document.getElementById("expanded-nav");
+    if (showNavigation){
+        expandedNav.classList.remove('expanded-nav');
+        showNavigation = false;
+    }
+    else{
+        expandedNav.classList.add('expanded-nav');
+        showNavigation = true;
+    }
+}
+
+function updateNavLiCSS(li){
+    let id = li.id;
+    if (!id) return;
+    if (id == "home") id = "index"
+    let location = document.location.href;
+    if (location.includes(id)) addSelectedClass(li);
+    else removeSelectedClass(li)
+}
+
+function addSelectedClass(el){
+    el.classList.add("selected");
+}
+
+function removeSelectedClass(el){
+    el.classList.remove("selected");
 }
 
 //////////////////////cursors
@@ -54,9 +98,11 @@ function trackCursor(event){
         }
     }
     setTimeout(()=>{
-    cursorTrail.style.left = event.pageX + "px";
-    cursorTrail.style.top = event.pageY + "px";
-    //makeTrail();
+        if (cursorTrail.classList.contains('opacity-0')){
+            cursorTrail.classList.remove('opacity-0')
+        }
+        cursorTrail.style.left = event.pageX + "px";
+        cursorTrail.style.top = event.pageY + "px";
     }, cursorTrailDelay);
     removeAnimationOnStationary(event);
 }
@@ -70,19 +116,10 @@ function removeAnimationOnStationary(event){
     }, cursorTrailDelay);
 }
 
-function makeTrail(){
-    if (!cursorTrail) return;
-    if (extendedTrail.length == 0){
-        let el = cursorTrail;
-        //el.id = "cursor-trail-1";
-        //document.body.append(el);
-        extendedTrail.push(el.id);
-    }
-}
-
 //////////////home slideshow
 function initHomeSlides(slideshow){
     let slides = document.querySelectorAll(`#${slideshow.id} .home-slide`);
+    if (!slides) return;
     //for css animation: opacity chng start = 100 - delay/time*100; animation duration = time
     var i = 0, time = 6000, delay = 1000; 
     slides[i].classList.add('active')
